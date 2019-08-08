6627bb8e6e3aaf5b874bc753c8197a269a8610c8
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-08 02:18:44 -0300

    Permite o acesso de terceiros

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Captura as rotas definidas
 const routes = require('./routes');
@@ -11,6 +12,8 @@ const port = 3333;  // Define a porta do servidor
 // Define a conexão com o banco de dados
 mongoose.set('useNewUrlParser', true);
 mongoose.connect('mongodb+srv://tinderdev:tinderdev@cluster0-msvvd.mongodb.net/tinderdev?retryWrites=true&w=majority')
+
+server.use(cors());
 
 // Define que o servidor deve esperar dados no formato JSON
 server.use(express.json());
