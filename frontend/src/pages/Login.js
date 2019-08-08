43dfa1a791c85aeec514dadd20a37842bd0b08f7
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-08 19:10:01 -0300

    Adicionada página principal da aplicação

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../services/api';
 
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/devs', { username });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev Logo" />
         <input
           placeholder="Digite seu usuário no GitHub"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
         />
         <button type="submit">Enviar</button>
       </form>
